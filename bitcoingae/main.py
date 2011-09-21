0922f1ffb7079d3092eb69358ec74d50f8b00d43
Stephen Cagle <samedhi@gmail.com>
2011-09-20 23:23:53 -0500

    Client is stable now

--- bitcoingae/main.py
+++ bitcoingae/main.py
@@ -3,6 +3,8 @@ from google.appengine.ext.webapp import util
 from google.appengine.api import urlfetch
 from google.appengine.api import taskqueue
 from google.appengine.api import channel
+from google.appengine.api import memcache
+from time import time
 import logging
 
 import simplejson
@@ -22,19 +24,28 @@ class Channel(db.Model):
 
 class RetrieveHandler(webapp.RequestHandler):
 		def get(self):
-			global globHighId
+			global globHighId, globLast
 			history = get_history()
-			result_gox = urlfetch.fetch('https://mtgox.com/code/data/ticker.php')
+			result_gox = result_trd = None
+			try:
+				result_gox = urlfetch.fetch('https://mtgox.com/code/data/ticker.php')
+			except DeadlineExceededError:
+				pass
 			if result_gox.status_code == 200:
 				json = simplejson.loads(result_gox.content)
 				last = float(json['ticker']['last'])
 				globLast['MtGox'] = last
-				logging.info("from Mt Gox, received:"+str(last))
+				logging.info("from MtGox, received:"+str(last))
 				history.mtGox = history.mtGox[1:1440] + [last] 
 			else:
-				logging.info("status code for Mt Gox was %s"%result_gox.status_code)
-				
-			result_trd = urlfetch.fetch('https://api.tradehill.com/APIv1/USD/Ticker')
+				globLast['MtGox'] = history.mtGox[-1]
+				history.mtGox = history.mtGox[1:1440] + history.mtGox[-1] 
+				logging.warning("MtGox retrieval failed %s"%result_gox.status_code)
+			
+			try:
+				result_trd = urlfetch.fetch('https://api.tradehill.com/APIv1/USD/Ticker')
+			except DeadlineExceededError:
+				pass
 			if result_trd.status_code == 200:
 				json = simplejson.loads(result_trd.content)
 				last = float(json['ticker']['last'])
@@ -42,7 +53,9 @@ class RetrieveHandler(webapp.RequestHandler):
 				logging.info("from TradeHill, received:"+str(last))
 				history.tradeHill = history.tradeHill[1:1440] + [last]
 			else:
-				logging.info("status code TradeHill was %s"%result_trd.status_code)
+				globLast['TradeHill'] = history.tradeHill[-1]
+				history.tradeHill = history.tradeHill[1:1440] + history.tradeHill[-1]
+				logging.warning("TradeHill retrieval failed %s"%result_trd.status_code)
 			
 			history.put()
 			taskqueue.add(url="/tasks/notify", params={'min': 0, 'max':globHighId, 'last': lastTick()})
@@ -77,19 +90,36 @@ def get_history():
 		
 class GetIdHandler(webapp.RequestHandler):
     def get(self):
-			while(True):
-				id = random.randint(0, globHighId - 1)
-				#not thread safe, but good enough
-				if not Channel.all().filter("id = ", id).get():
-					break
-			token = channel.create_channel(str(id))
-			self.response.out.write('{ "id" : %s , "token" : "%s", "mtgoxHist" : %s , "tradeHist" : %s}' % 
-				(id, token, get_history().mtGox, get_history().tradeHill))
+			banned = memcache.get(self.request.remote_addr+"Banned")
+			until = memcache.get(self.request.remote_addr+"Until")
+			t = time()
+			
+			if not banned:
+				banned = t
+				until  = 1
+				memcache.set(self.request.remote_addr+"Banned", banned, 60 * 15)
+				memcache.set(self.request.remote_addr+"Until", until, 60*15)
+			elif t < banned + until: 
+				if until < 1024:
+					until *= 2
+					memcache.set(self.request.remote_addr+"Until", until, 60*15)
+			
+			if t + 1 < banned + until: #+1 to ignore the first ban
+				self.response.out.write('{"msg":"Over taxing resources", "penalty_box_until":%s}'%(banned+until))
+			else:
+				while(True):
+					id = random.randint(0, globHighId - 1)
+					#not thread safe, but good enough
+					if not Channel.all().filter("id = ", id).get(): break
+				token = channel.create_channel(str(id))
+				self.response.out.write('{ "id" : %s , "token" : "%s", "mtgoxHist" : %s , "tradeHist" : %s}' % 
+					(id, token, get_history().mtGox, get_history().tradeHill))
 
 class ConnectedHandler(webapp.RequestHandler):
 	def post(self):
 		global globLast
 		client_id = int(self.request.get('from'))
+		logging.info("the client id was %s" % client_id)
 		Channel(id=client_id).put()
 		taskqueue.add(url="/tasks/notify", params={'min': client_id, 
 																							 'max': client_id + 1, 
@@ -98,6 +128,7 @@ class ConnectedHandler(webapp.RequestHandler):
 class DisconnectedHandler(webapp.RequestHandler):
 	def post(self):
 		client_id = int(self.request.get('from'))
+		logging.info("the client id disconnecting was %s" % client_id)
 		Channel.all().filter("id = ", client_id).get().delete()
 		
 class DisconnectedWorker(webapp.RequestHandler):
